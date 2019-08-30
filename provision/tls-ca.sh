9bca900b6e3ab26f6ffd55b6c7bc6dcf758ea092
Benjamin Lu <benlumia007@gmail.com>
2019-08-30 08:05:12 -0700

    3650 days is roughly around 10 years, seems too long, so changed to 365 days seems acceptable.

--- provision/tls-ca.sh
+++ provision/tls-ca.sh
@@ -22,6 +22,6 @@ for domain in `get_sites`; do
 
         openssl genrsa -out "certificates/${domain}/${domain}.key" 4096 &> /dev/null
         openssl req -new -key "certificates/${domain}/${domain}.key" -out "certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
-        openssl x509 -req -in "certificates/${domain}/${domain}.csr" -CA "certificates/ca/ca.crt" -CAkey "certificates/ca/ca.key" -CAcreateserial -out "certificates/${domain}/${domain}.crt" -days 3650 -sha256 -extfile "certificates/${domain}/${domain}.ext" &> /dev/null
+        openssl x509 -req -in "certificates/${domain}/${domain}.csr" -CA "certificates/ca/ca.crt" -CAkey "certificates/ca/ca.key" -CAcreateserial -out "certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "certificates/${domain}/${domain}.ext" &> /dev/null
     fi
 done
