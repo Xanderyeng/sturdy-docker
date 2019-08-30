6c1aba322c78d054e47c8f8323b54a639170f5a7
Benjamin Lu <benlumia007@gmail.com>
2019-08-30 00:05:03 -0700

    initial commit

---
+++ provision/initial-setup.sh
@@ -0,0 +1,40 @@
+
+#!/bin/bash
+
+config="config/docker-setup.yml"
+
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
+
+for domain in `get_sites`; do
+
+    if [[ ! -f "config/nginx/${domain}" ]]; then
+        cp "config/nginx/nginx.tmpl" "config/nginx/${domain}.conf"
+    fi
+
+    get_hosts() {
+        local value=`cat ${config} | shyaml get-value sites.${domain}.host`
+        echo ${value:$@}
+    }
+
+    get_address() {
+        local value=`cat ${config} | shyaml get-value sites.${domain}.address`
+        echo ${value:$@}
+    }
+
+    for host in `get_hosts`; do 
+        if grep -q "{{DOMAIN}}" "config/nginx/${domain}.conf"; then
+            sed -i -e "s/{{DOMAIN}}/${host}/g" "config/nginx/${domain}.conf"
+            rm -rf "config/nginx/${domain}.conf-e"
+        fi
+    done
+
+    for address in `get_address`; do
+        if grep -q "{{IP_ADDRESS}}" "config/nginx/${domain}.conf"; then
+            sed -i -e "s/{{IP_ADDRESS}}/${address}/g" "config/nginx/${domain}.conf"
+            rm -rf "config/nginx/${domain}.conf-e"
+        fi
+    done
+done
