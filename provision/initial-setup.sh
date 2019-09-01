c7fd35b3aff40b99494a8eae5fd6bfe7cade4a9c
Benjamin Lu <benlumia007@gmail.com>
2019-08-31 20:32:43 -0700

    if docker-custom.yml doesn't exist then copy docker-setup.yml to docker-custom.yml, which allows users to edit this instead of docker-setup.yml

--- provision/initial-setup.sh
+++ provision/initial-setup.sh
@@ -1,6 +1,10 @@
 
 #!/bin/bash
 
+if [[ ! -f "config/docker-custom.yml" ]]; then
+    cp "config/docker-setup.yml" "config/docker-custom.yml"
+fi
+
 config="config/docker-custom.yml"
 
 get_sites() {
