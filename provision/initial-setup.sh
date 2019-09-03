ea7103f8341fb9358a33678125962dd9a3460d10
Benjamin Lu <benlumia007@gmail.com>
2019-09-03 11:32:36 -0700

    updted

--- provision/initial-setup.sh
+++ provision/initial-setup.sh
@@ -3,6 +3,7 @@
 
 if [[ ! -f "config/docker-custom.yml" ]]; then
     cp "config/docker-setup.yml" "config/docker-custom.yml"
+    cp "config/docker-compose-setup.yml" "docker-compose.yml"
 fi
 
 config="config/docker-custom.yml"
