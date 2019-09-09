fe4446d027f119a5eea9bc07a812797e8b38973c
Benjamin Lu <benlumia007@gmail.com>
2019-09-08 18:01:53 -0700

    copy from tempates to the correct location

--- provision/initial-setup.sh
+++ provision/initial-setup.sh
@@ -2,5 +2,5 @@
 
 if [[ ! -f "config/docker-custom.yml" ]]; then
     cp "config/docker-setup.yml" "config/docker-custom.yml"
-    cp "config/docker-compose-setup.yml" "docker-compose.yml"
+    cp "templates/docker-compose.tmpl" "docker-compose.yml"
 fi
