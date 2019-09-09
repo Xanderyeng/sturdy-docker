e1bffb3d1b189bd1af1996da8be85f66d6d61191
Benjamin Lu <benlumia007@gmail.com>
2019-09-09 14:46:07 -0700

    changed file loctions to templates folder

--- provision/initial-setup.sh
+++ provision/initial-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ ! -f "config/docker-custom.yml" ]]; then
-    cp "config/docker-setup.yml" "config/docker-custom.yml"
-    cp "templates/docker-compose.tmpl" "docker-compose.yml"
+    cp "templates/docker-setup.yml" "config/docker-custom.yml"
+    cp "templates/docker-compose.yml" "docker-compose.yml"
 fi
