bdd3f1741ca59a1d4daf202153abdb4278825f22
Benjamin Lu <benlumia007@gmail.com>
2019-09-09 14:46:58 -0700

    added provision to true so that it creates the site, this allows remove-sites to be used if provision is set to false

--- provision/create-sites.sh
+++ provision/create-sites.sh
@@ -8,31 +8,33 @@ get_sites() {
 }
 
 for domain in `get_sites`; do
+    provision=`cat ${config} | shyaml get-value sites.${domain}.provision`
 
-    if [[ ! -f "config/nginx/${domain}" ]]; then
-        cp "templates/nginx.tmpl" "config/nginx/${domain}.conf"
-        if grep -q "{{DOMAIN}}" "config/nginx/${domain}.conf"; then
-            sed -i -e "s/{{DOMAIN}}/${domain}/g" "config/nginx/${domain}.conf"
-            rm -rf "config/nginx/${domain}.conf-e"
+    if [[ "True" == ${provision} ]]; then
+        if [[ ! -f "config/nginx/${domain}.conf" ]]; then
+            cp "templates/nginx.conf" "config/nginx/${domain}.conf"
+
+            if grep -q "{{DOMAIN}}" "config/nginx/${domain}.conf"; then
+                sed -i -e "s/{{DOMAIN}}/${domain}/g" "config/nginx/${domain}.conf"
+                rm -rf "config/nginx/${domain}.conf-e"
+            fi
+            mkdir -p "sites/${domain}"
         fi
-        mkdir -p "sites/${domain}"
+
+        get_hosts() {
+            local value=`cat ${config} | shyaml get-value sites.${domain}.host`
+            echo ${value:$@}
+        }
+
+        for host in `get_hosts`; do 
+            if grep -q "{{HOST}}" "config/nginx/${domain}.conf"; then
+                sed -i -e "s/{{HOST}}/${host}/g" "config/nginx/${domain}.conf"
+                rm -rf "config/nginx/${domain}.conf-e"
+            fi
+
+            if ! grep -q "${host}" /etc/hosts; then
+            echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
+            fi
+        done
     fi
-
-    get_hosts() {
-        local value=`cat ${config} | shyaml get-value sites.${domain}.host`
-        echo ${value:$@}
-    }
-
-    for host in `get_hosts`; do 
-        if grep -q "{{HOST}}" "config/nginx/${domain}.conf"; then
-            sed -i -e "s/{{HOST}}/${host}/g" "config/nginx/${domain}.conf"
-            rm -rf "config/nginx/${domain}.conf-e"
-        fi
-
-        if ! grep -q "${host}" /etc/hosts; then
-           echo "127.0.0.1     ${domain}.test" | sudo tee -a /etc/hosts
-        else
-            echo "${host} already exists in /etc/hosts"
-        fi
-    done
 done
