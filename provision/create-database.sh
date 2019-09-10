dc0c7a7116ba6db68e65116a25d1b3f710ad0119
Benjamin Lu <benlumia007@gmail.com>
2019-09-10 00:56:37 -0700

    add wp core install

--- provision/create-database.sh
+++ provision/create-database.sh
@@ -12,4 +12,6 @@ for domain in `get_sites`; do
     docker exec -it docker-mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"
     docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
     docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
+
+    docker exec -it docker-sandbox wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --allow-root
 done
