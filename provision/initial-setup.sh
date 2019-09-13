cec98f3dd662a29da0acf7cf8dd8cf59721f056d
Benjamin Lu <benlumia007@gmail.com>
2019-09-12 23:01:42 -0700

    delete initial setup.sh

--- provision/initial-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ ! -f "config/docker-custom.yml" ]]; then
-    cp "templates/docker-setup.yml" "config/docker-custom.yml"
-    cp "templates/docker-compose.yml" "docker-compose.yml"
-fi
