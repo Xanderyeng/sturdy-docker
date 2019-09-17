1ada9c2e11c5246dcf60dc8ccdc4f1482ce03616
Benjamin Lu <benlumia007@gmail.com>
2019-09-16 18:18:34 -0700

    mvoed fiels to config/templates

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ ! -f "config/docker-custom.yml" ]]; then
-    cp "templates/docker-setup.yml" "config/docker-custom.yml"
-    cp "templates/docker-compose.yml" "docker-compose.yml"
+    cp "config/templates/docker-setup.yml" "config/docker-custom.yml"
+    cp "config/templates/docker-compose.yml" "docker-compose.yml"
 fi
 
 config="config/docker-custom.yml"
