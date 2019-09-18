d77d30d56737a2e5e074e5445348c89ed7f19cfb
Benjamin Lu <benlumia007@gmail.com>
2019-09-18 08:06:54 -0700

    if is false then do nothing for now

--- provision/sites.sh
+++ provision/sites.sh
@@ -34,25 +34,5 @@ for domain in `get_sites`; do
                 source ${dir}/provision/setup.sh
             fi
         fi
-    else
-        rm -rf "certificates/${domain}.crt"
-        rm -rf "certificates/${domain}.key"
-        rm -rf "certificates/${domain}.csr"
-        rm -rf "certificates/${domain}.ext"
-        rm -rf "config/nginx/${domain}.conf"
-
-        if grep -q "${domain}.test" /etc/hosts; then
-            uname=`uname`
-            if [[ ${uname} == "Linux" ]]; then
-                sudo sed -i "/${domain}.test/d" "/etc/hosts"
-            else
-                sudo sed -i '' "/${domain}.test/d" "/etc/hosts"
-            fi
-        fi
-
-        docker exec -it docker-mysql mysql -u root -e "DROP DATABASE IF EXISTS ${domain};"
-        docker exec -it docker-mysql mysql -u root -e "REVOKE ALL PRIVILEGES ON ${domain}.* FROM 'wordpress'@'%';"
-        docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
-        rm -rf "sites/${domain}"
     fi
 done
