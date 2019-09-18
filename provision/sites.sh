2704ef3c5daa4fd1a6d616de0dd8d265b823a39f
Benjamin Lu <benlumia007@gmail.com>
2019-09-17 20:16:31 -0700

    add sites from setup

--- provision/sites.sh
+++ provision/sites.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
 
-repo="https://github.com/benlumia007/docker-for-wordpress-sites.git"
-dir="sites/${sites}"
+config="config/docker-custom.yml"
 
-if [[ false != "${repo}" ]]; then
-    if [[ ! -d "${dir}/provision/.git" ]]; then
-        git clone ${repo} ${dir}/provision -q
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
+
+for sites in `get_sites`; do
+    get_site_provision() {
+        local value=`cat ${config} | shyaml get-value sites.${sites}.provision 2> /dev/null`
+        echo ${value:-$@}
+    }
+
+    provision=`get_site_provision`
+
+    if [[ "True" == ${provision} ]]; then
+        repo="https://github.com/benlumia007/docker-for-wordpress-sites.git"
+        dir="sites/${sites}"
+
+        if [[ false != "${repo}" ]]; then
+            if [[ ! -d "${dir}/provision/.git" ]]; then
+                git clone ${repo} ${dir}/provision -q
+            else
+                cd "${dir}/provision"
+                git pull origin master -q
+                cd ../../..
+            fi
+        fi
+
+        if [[ -d ${dir} ]]; then
+            if [[ -f ${dir}/provision/setup.sh ]]; then
+                source ${dir}/provision/setup.sh
+            fi
+        fi
     else
-        cd "${dir}/provision"
-        git pull origin master -q
-        cd ../../..
-    fi
-fi
+        rm -rf "certificates/${domain}.crt"
+        rm -rf "certificates/${domain}.key"
+        rm -rf "certificates/${domain}.csr"
+        rm -rf "certificates/${domain}.ext"
+        rm -rf "config/nginx/${domain}.conf"
 
-if [[ -d ${dir} ]]; then
-    if [[ -f ${dir}/provision/setup.sh ]]; then
-        source ${dir}/provision/setup.sh
+        if grep -q "${domain}.test" /etc/hosts; then
+            uname=`uname`
+            if [[ ${uname} == "Linux" ]]; then
+                sudo sed -i "/${domain}.test/d" "/etc/hosts"
+            else
+                sudo sed -i '' "/${domain}.test/d" "/etc/hosts"
+            fi
+        fi
+
+        docker exec -it docker-mysql mysql -u root -e "DROP DATABASE IF EXISTS ${domain};"
+        docker exec -it docker-mysql mysql -u root -e "REVOKE ALL PRIVILEGES ON ${domain}.* FROM 'wordpress'@'%';"
+        docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
+        rm -rf "sites/${domain}"
     fi
-fi
+done
