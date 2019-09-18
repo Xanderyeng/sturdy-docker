f39a15046a668a462aee98ade728fa4dcc50f2eb
Benjamin Lu <benlumia007@gmail.com>
2019-09-17 20:16:01 -0700

    only copy main files

--- provision/setup.sh
+++ provision/setup.sh
@@ -4,27 +4,3 @@ if [[ ! -f "config/docker-custom.yml" ]]; then
     cp "config/templates/docker-setup.yml" "config/docker-custom.yml"
     cp "config/templates/docker-compose.yml" "docker-compose.yml"
 fi
-
-config="config/docker-custom.yml"
-
-get_sites() {
-    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
-    echo ${value:-$@}
-}
-
-for sites in `get_sites`; do
-    get_site_provision() {
-        local value=`cat ${config} | shyaml get-value sites.${sites}.provision 2> /dev/null`
-        echo ${value:-$@}
-    }
-
-    provision=`get_site_provision`
-
-    if [[ "True" == ${provision} ]]; then
-        source provision/sites.sh
-    else
-        source provision/remove-sites.sh
-    fi
-done
-
-
