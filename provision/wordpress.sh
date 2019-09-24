688707e5b34a3c5b30979b897bc4525262b990a5
Benjamin Lu <benlumia007@gmail.com>
2019-09-24 08:39:26 -0700

    changed the site_title for wp cli

--- provision/wordpress.sh
+++ provision/wordpress.sh
@@ -10,7 +10,7 @@ get_sites() {
 
 for domain in `get_sites`; do
     dir="sites/${domain}/public_html"
-    
+
     get_site_provision() {
         local value=`cat ${config} | shyaml get-value sites.${domain}.provision 2> /dev/null`
         echo ${value:-$@}
@@ -34,7 +34,7 @@ for domain in `get_sites`; do
             docker exec -it docker-mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"
             docker exec -it docker-mysql mysql -u root -e "FLUSH PRIVILEGES;"
 
-            docker exec -it docker-phpfpm wp core install  --url="https://${domain}.test" --title="${site_title}" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --path=/var/www/html/${domain}/public_html --allow-root
+            docker exec -it docker-phpfpm wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --path=/var/www/html/${domain}/public_html --allow-root
             docker exec -it docker-phpfpm wp plugin delete akismet --path=/var/www/html/${domain}/public_html --allow-root
             docker exec -it docker-phpfpm wp plugin delete hello --path=/var/www/html/${domain}/public_html --allow-root
             docker exec -it docker-phpfpm wp config shuffle-salts --path=/var/www/html/${domain}/public_html --allow-root
