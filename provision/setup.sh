24fb5834d49530a4b2f82072e1f4ebb937b40e45
Benjamin Lu <benlumia007@gmail.com>
2019-09-26 11:55:50 -0700

    removed file

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,5 +2,4 @@
 
 if [[ ! -f "config/docker-custom.yml" ]]; then
     cp "config/templates/docker-setup.yml" "config/docker-custom.yml"
-    cp "config/templates/docker-compose.yml" "docker-compose.yml"
 fi
