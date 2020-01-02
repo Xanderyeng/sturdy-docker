df360392b874e633e8635f29e5a80ac0bb015799
Benjamin Lu <benlumia007@gmail.com>
2020-01-02 14:12:11 -0800

    removed jetpack define

--- config/templates/wp-config.php
+++ config/templates/wp-config.php
@@ -78,7 +78,6 @@ $table_prefix = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define( 'WP_DEBUG', true );
-define( 'JETPACK_DEV_DEBUG', true );
 define( 'DISALLOW_FILE_EDIT', true );
 
 /* That's all, stop editing! Happy publishing. */
