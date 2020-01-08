1740ecc7acb0701ea2927008e8381789646444ce
Benjamin Lu <benlumia007@gmail.com>
2020-01-07 19:57:35 -0800

    ignore readme.md and remove mkdir

--- provision/backup-database.sh
+++ provision/backup-database.sh
@@ -3,16 +3,14 @@
 config="config/docker-custom.yml"
 
 get_sites() {
-    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
-    echo ${value:-$@}
+  local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+  echo ${value:-$@}
 }
 
 db_backups=`cat ${config} | shyaml get-value options.db_backups 2> /dev/null`
 
 if [[ ${db_backups} != "False" ]]; then
-    mkdir -p "database/backups"
-
-    for domain in `get_sites`; do
-      docker exec -it docker-mysql mysqldump -u root "${domain}" > "database/backups/${domain}.sql"
-    done
+  for domain in `get_sites`; do
+    docker exec -it docker-mysql mysqldump -u root "${domain}" > "database/${domain}.sql"
+  done
 fi
