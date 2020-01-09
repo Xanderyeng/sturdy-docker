9ebdae7088916fd37bce55acdba8515e04eeecb4
Benjamin Lu <benlumia007@gmail.com>
2020-01-09 14:46:15 -0800

    add an echo if backup's are successful

--- provision/backup-database.sh
+++ provision/backup-database.sh
@@ -12,5 +12,7 @@ db_backups=`cat ${config} | shyaml get-value options.db_backups 2> /dev/null`
 if [[ ${db_backups} != "False" ]]; then
   for domain in `get_sites`; do
     docker exec -it docker-mysql mysqldump -u root "${domain}" > "database/${domain}.sql"
+    bold=$(tput bold)
+    echo -e "\e[33m${bold}Backing Up Database ${domain}.\e[0m"
   done
 fi
