9863de501fb1c04594871d3b12994c4e01cba75f
Benjamin Lu <benlumia007@gmail.com>
2020-01-09 23:50:44 -0800

    readjust the code

--- provision/backup-database.sh
+++ provision/backup-database.sh
@@ -10,9 +10,7 @@ get_sites() {
 db_backups=`cat ${config} | shyaml get-value options.db_backups 2> /dev/null`
 
 if [[ ${db_backups} != "False" ]]; then
-  for domain in `get_sites`; do
-    docker exec -it docker-mysql mysqldump -u root "${domain}" > "database/${domain}.sql"
-    bold=$(tput bold)
-    echo -e "\e[33m${bold}Backing Up Database ${domain}.\e[0m"
-  done
+	for domain in `get_sites`; do
+		docker exec -it docker-mysql mysqldump -u root "${domain}" > "database/${domain}.sql"
+	done
 fi
