966b19bc7927fb125e1b5439a6a40e8952903f44
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-21 09:27:11 -0700

    check if is on macOS

--- provision/dashboard.sh
+++ provision/dashboard.sh
@@ -10,11 +10,13 @@ if [[ ! -d "${dir}" ]]; then
     sed -i -e "s/{{DOMAIN}}/dashboard.test/g" "config/nginx/dashboard.conf"
     rm -rf "config/nginx/dashboard.conf-e"
 
+    macOS=`uname`
+
     if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
         if ! grep -q "dashboard.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
             echo "127.0.0.1   dashboard.test" | sudo tee -a /mnt/c/Windows/System32/drivers/etc/hosts
         fi
-    else
+    elif ${macOS} == "Darwin" ; then
         if ! grep -q "dashboard.test" /etc/hosts; then
             echo "127.0.0.1   dashboard.test" | sudo tee -a /etc/hosts
         fi
