d1d71b4f319ee946c539b6cf59e8c21db23f64af
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-24 17:26:48 -0700

    allow to switch between diffrent php versions if necessary

--- provision/dashboard.sh
+++ provision/dashboard.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+config=".global/docker-custom.yml"
+
 repo="https://github.com/benlumia007/docker-for-wordpress-dashboard.git"
 dir="sites/dashboard/public_html"
+
+get_preprocessor() {
+    local value=`cat ${config} | shyaml get-value preprocessor 2> /dev/null`
+    echo ${value:-$@}
+}
 
 if [[ ! -d "${dir}" ]]; then
     mkdir -p "config/nginx"
@@ -20,6 +27,38 @@ if [[ ! -d "${dir}" ]]; then
         fi
     fi
 fi
+
+preprocessors=`get_preprocessor`
+
+for php in ${preprocessors//- /$'\n'}; do
+
+    if [[ ${php} == "7.2" ]]; then
+        if grep -q "7.3" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.4" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    elif [[ ${php} == "7.3" ]]; then
+        if grep -q "7.2" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.4" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    elif [[ ${php} == "7.4" ]]; then
+        if grep -q "7.2" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.3" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    fi
+
+done
 
 if [[ false != "${repo}" ]]; then
     if [[ ! -d ${dir}/.git ]]; then
