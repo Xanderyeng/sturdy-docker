f6128a23161ab839162a7ff6e1f40f358d69eac3
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-26 21:54:56 -0700

    this works, but to be able to delete hosts from windows using wsl2, must run as administrator to do it.

---
+++ provision/delete.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+config="../.global/docker-custom.yml"
+
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
+
+cd sites
+for domain in $( ls ); do
+    if ! grep -q ${domain} ${config}; then
+        if [[ "dashboard" != ${domain} ]]; then 
+            rm -rf ${domain}
+            rm -rf ../certificates/${domain}
+            rm -rf ../config/nginx/${domain}.conf
+
+            if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+                if grep -q "${domain}.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
+                    sed -i "/${domain}.test/d" /mnt/c/Windows/System32/drivers/etc/hosts
+                fi
+            else
+                if grep -q "${domain}.test" /etc/hosts; then
+                    sed -i "/${domain}.test/d" /etc/hosts
+                fi
+            fi
+        fi
+    fi
+
+done
