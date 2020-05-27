68dcef2db7d48419e9e229511d705f8f43f09954
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-27 11:16:54 -0700

    updated

--- provision/setup.sh
+++ provision/setup.sh
@@ -3,6 +3,7 @@
 config=".global/docker-custom.yml"
 
 if [[ ! -f ".global/docker-custom.yml" ]]; then
+    cp "config/templates/docker-compose.tmpl" ".global/docker-compose.yml"
     cp "config/templates/docker-setup.yml" ".global/docker-custom.yml"
 fi
 
