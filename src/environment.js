b47e3809b4a740f872ed54e7ea665f4861b46050
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-28 18:21:49 -0700

    add setRootPath and exports

--- src/environment.js
+++ src/environment.js
@@ -2,8 +2,15 @@
 
 const path = require( "path" );
 const rootPath = path.dirname( require.main.filename );
+const certsPath = path.join( `${rootPath}/certificates` );
 const configPath = path.join( `${rootPath}/config` );
 const databasePath = path.join( `${rootPath}/databases` );
 const logsPath = path.join( `${rootPath}/logs` );
 const srcPath = path.join( `${rootPath}/src` );
 const sitesPath = path.join( `${rootPath}/sites` );
+
+const setRootPath = function() {
+    return rootPath;
+}
+
+module.exports = { setRootPath };
