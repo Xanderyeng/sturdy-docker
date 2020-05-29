4025f9ad8b2f0e0595367f30dc16e8d1a26a9379
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-28 18:15:51 -0700

    set databasesPath

--- src/environment.js
+++ src/environment.js
@@ -3,6 +3,7 @@
 const path = require( "path" );
 const rootPath = path.dirname( require.main.filename );
 const configPath = path.join( `${rootPath}/config` );
+const databasePath = path.join( `${rootPath}/databases` );
 const logsPath = path.join( `${rootPath}/logs` );
 const srcPath = path.join( `${rootPath}/src` );
 const sitesPath = path.join( `${rootPath}/sites` );
