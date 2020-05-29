68cfb3b6e4781df88e248edf134df7c368df54f6
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-28 18:12:52 -0700

    set sitesPath

--- src/environment.js
+++ src/environment.js
@@ -4,4 +4,4 @@ const path = require( "path" );
 const rootPath = path.dirname( require.main.filename );
 const srcPath = path.join( `${rootPath}/src` );
 const configPath = path.join( `${rootPath}/config` );
-
+const sitesPath = path.join( `${rootPath}/sites` );
