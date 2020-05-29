eb3a33d2e43781a3769954c71cd1700484c825e9
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-29 11:33:25 -0700

    add php version during provisioning

--- scripts/dashboard.sh
+++ scripts/dashboard.sh
@@ -2,7 +2,19 @@
 config=${PWD}/.global/docker-custom.yml
 dir=${PWD}/sites/dashboard/public_html
 
-repo="https://github.com/benlumia007/docker-for-wordpress-dashboard.git"
+
+get_dashboard_repo() {
+    local value=`cat ${config} | shyaml get-value default.repo`
+    echo ${value:$@}
+}
+
+get_php_version() {
+    local value=`cat ${config} | shyaml get-value preprocessor`
+    echo ${value:$@}
+}
+
+repo=`get_dashboard_repo`
+preprocessors=`get_php_version`
 
 if [[ false != "${repo}" ]]; then
     if [[ ! -d ${dir}/.git ]]; then
@@ -13,3 +25,32 @@ if [[ false != "${repo}" ]]; then
         cd ../../..
     fi
 fi
+
+for php in ${preprocessors//- /$'\n'}; do
+    if [[ ${php} == "7.2" ]]; then
+        if grep -q "7.3" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.4" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    elif [[ ${php} == "7.3" ]]; then
+        if grep -q "7.2" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.4" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.4/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    elif [[ ${php} == "7.4" ]]; then
+        if grep -q "7.2" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.2/${php}/g" "config/nginx/dashboard.conf"
+        elif grep -q "7.3" config/nginx/dashboard.conf; then
+            sed -i -e "s/7.3/${php}/g" "config/nginx/dashboard.conf"
+        else
+            sed -i -e "s/{{PHPVERSION}}/${php}/g" "config/nginx/dashboard.conf"
+        fi
+    fi
+
+done
