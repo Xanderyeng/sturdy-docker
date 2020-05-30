4d9a58dc668a27077ae44584b2e5356a3ac940e6
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-29 20:08:56 -0700

    fixed backups

--- scripts/backup-database.sh
+++ scripts/backup-database.sh
@@ -3,14 +3,16 @@
 config=".global/docker-custom.yml"
 
 get_sites() {
-  local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+  local value=`cat ${config} | shyaml get-value sites.domain 2> /dev/null`
   echo ${value:-$@}
 }
 
 db_backups=`cat ${config} | shyaml get-value options.db_backups 2> /dev/null`
 
+domains=`get_sites`
+
 if [[ ${db_backups} != "False" ]]; then
-	for domain in `get_sites`; do
+	for domain in ${domains//- /$'\n'}; do
 		docker exec -i docker-mysql mysqldump -u root "${domain}" > "database/${domain}.sql"
 	done
 fi
