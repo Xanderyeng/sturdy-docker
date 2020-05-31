8eee92cb52cabe0dd79ce210fd97bbe1d67aa676
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-05-30 18:15:10 -0700

    removed script setup

--- scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-config=".global/docker-custom.yml"
-
-if [[ ! -f ".global/docker-custom.yml" ]]; then
-    cp "config/templates/docker-compose.yml" ".global/docker-compose.yml"
-    cp "config/templates/docker-setup.yml" ".global/docker-custom.yml"
-fi
