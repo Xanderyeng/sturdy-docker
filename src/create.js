13dd629b2cd92840b30ad948b4ec48dc3ebfb712
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-04 00:41:37 -0700

    creating create.js  to create WordPress sites

---
+++ src/create.js
@@ -0,0 +1,15 @@
+const path = require( './configure' );
+const getRootPath = path.setRootPath();
+
+const help = function() {
+	let help = `
+
+
+`
+};
+
+const command = async function() {
+
+};
+
+module.exports = { command };
