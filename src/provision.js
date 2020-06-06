05eb025197b4b832422f2fb20c7f805a10ac9973
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-06 12:59:08 -0700

    switch to apache and remove php versions

--- src/provision.js
+++ src/provision.js
@@ -21,105 +21,86 @@ const shell = require( "shelljs" );
 const configuredHosts = require( "./hosts" );
 const getComposeFile = path.setComposeFile();
 const getCustomFile = path.setCusomFile();
+const { execSync } = require( 'child_process' );
+const { exec } = require( 'child_process' );
 
 const config = yaml.safeLoad( fs.readFileSync( `${getCustomFile}`, 'utf8' ) );
 
 // Here we are going to setup the actual sites
 const domains = config.sites.domain;
 const provision = config.sites.provision;
-const setDomainPHP = config.preprocessor;
 
 if ( provision == true ) {
     for ( const domain of domains ) {
-        if ( ! fs.existsSync( `${getSitesPath}/${domain}/public_html` ) ) {
-			shell.mkdir( `-p`, `${getSitesPath}/${domain}/public_html` );
-        }
+        if ( ! fs.existsSync( `${getConfigPath}/apache/${domain}.conf` ) ) {
+			fs.copy( `${getConfigPath}/templates/apache.conf`, `${getConfigPath}/apache/${domain}.conf`, error => {
+				if ( error ) {
+					throw error;
+				} else {
+					const options = { files: `${getConfigPath}/apache/${domain}.conf`, from: /{{DOMAIN}}/g, to: `${domain}` };
+					replaced = replace.sync( options )
 
-        if ( ! fs.existsSync( `${getConfigPath}/nginx/${domain}.conf` ) ) {
-			shell.cp( `-r`, `${getConfigPath}/templates/nginx.conf`, `${getConfigPath}/nginx/${domain}.conf` );
-			const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /{{DOMAIN}}/g, to: `${domain}` };
-			replaced = replace.sync( options );
-
-
-			if ( isWSL ) {
-				configuredHosts.set('127.0.0.1', `${domain}.test`, function (err) {
-					if (err) {
-					  console.error(err)
+					if ( isWSL ) {
+						configuredHosts.set('127.0.0.1', `${domain}.test`, function (err) {
+							if (err) {
+							  console.error(err)
+							} else {
+							  console.log('set /etc/hosts successfully!')
+							}
+						});
 					} else {
-					  console.log('set /etc/hosts successfully!')
+						exec( `sudo d4w-hosts set 127.0.0.1 ${domain}.test` );
 					}
-				});
-			} else {
-				shell.exec( `sudo d4w-hosts set 127.0.0.1 ${domain}.test` );
-			}
+
+					execSync( `docker-compose -f ${getComposeFile} exec -T nginx sudo a2ensite ${domain}` );
+				}
+			} );
 		}
 
-		for ( const getDomainPHP of setDomainPHP ) {
-			if ( getDomainPHP == "7.2" ) {
-				if ( shell.grep( `-i`, `7.3`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.3/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
+        if ( ! fs.existsSync( `${getSitesPath}/${domain}/public_html` ) ) {
+			fs.mkdir( `${getSitesPath}/${domain}/public_html`, { recursive: true }, error => {
+				if ( error ) {
+					throw error;
+				} else {
+					if ( ! fs.existsSync( `${getSitesPath}/${domain}/public_html/wp-config.php` ) ) {
+						const dir = `/srv/www/${domain}/public_html`;
 
-				if ( shell.grep( `-i`, `7.4`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.4/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
+						execSync( `docker-compose -f ${getComposeFile} exec -T mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T mysql mysql -u root -e "FLUSH PRIVILEGES;"` );
 
-				if ( shell.grep( `-i`, `{{PHPVERSION}}`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /{{PHPVERSION}}/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp core download --path=${dir}` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path=${dir}` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path=${dir}` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp plugin delete akismet --path=${dir}` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp plugin delete hello --path=${dir}` );
+						execSync( `docker-compose -f ${getComposeFile} exec -T nginx wp config shuffle-salts --path=${dir}` );
+					}
 				}
-			} else if ( getDomainPHP == "7.3" ) {
-				if ( shell.grep( `-i`, `7.2`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.2/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-
-				if ( shell.grep( `-i`, `7.4`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.4/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-
-				if ( shell.grep( `-i`, `{{PHPVERSION}}`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /{{PHPVERSION}}/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-			} else if ( getDomainPHP == "7.4" ) {
-				if ( shell.grep( `-i`, `7.2`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.2/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-
-				if ( shell.grep( `-i`, `7.3`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /7.3/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-
-				if ( shell.grep( `-i`, `{{PHPVERSION}}`, `${getConfigPath}/nginx/${domain}.conf` ) ) {
-					const options = { files: `${getConfigPath}/nginx/${domain}.conf`, from: /{{PHPVERSION}}/g, to: `${getDomainPHP}` };
-					replaced = replace.sync( options );
-				}
-			}
+			} );
 		}
 
-		if ( ! fs.existsSync( `${getSitesPath}/${domain}/public_html/wp-config.php` ) ) {
-			const dir = `/srv/www/${domain}/public_html`;
+		// Here, we are going to create certs for the ${domain}.
+		if ( ! fs.existsSync( `${getCertsPath}/${domain}/${domain}.crt` ) ) {
+			fs.mkdir( `${getCertsPath}/${domain}/`, { recursive: true }, error => {
+				if ( error ) {
+					throw error;
+				} else {
+					fs.copy( `${getConfigPath}/templates/certs.ext`, `${getCertsPath}/${domain}/${domain}.ext`, error => {
+						if ( error ) {
+							throw error;
+						} else {
+							const options = { files: `${getCertsPath}/${domain}/${domain}.ext`, from: /{{DOMAIN}}/g, to: `${domain}` };
+							replaced = replace.sync( options );
 
-			shell.exec( `docker-compose -f ${compose} exec -T mysql mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${domain};"` );
-			shell.exec( `docker-compose -f ${compose} exec -T mysql mysql -u root -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'wordpress';"` );
-			shell.exec( `docker-compose -f ${compose} exec -T mysql mysql -u root -e "GRANT ALL PRIVILEGES ON ${domain}.* to 'wordpress'@'%' WITH GRANT OPTION;"` );
-
-			shell.exec( `docker-compose -f ${compose} exec -T mysql mysql -u root -e "FLUSH PRIVILEGES;"` );
-
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp core download --path="${dir}" --quiet --allow-root` );
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp config create --dbhost=mysql --dbname=${domain} --dbuser=wordpress --dbpass=wordpress --path="${dir}" --quiet --allow-root` );
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp core install  --url="https://${domain}.test" --title="${domain}.test" --admin_user=admin --admin_password=password --admin_email="admin@${domain}.test" --skip-email --quiet --path=${dir} --allow-root` );
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp plugin delete akismet --path=${dir} --quiet --allow-root` );
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp plugin delete hello --path=${dir} --quiet --allow-root` );
-			shell.exec( `docker-compose -f ${compose} exec -T nginx wp config shuffle-salts --path=${dir} --quiet --allow-root` );
-
-			shell.exec( `docker-compose -f ${compose} exec -T nginx chown -R 1000:1000 ${dir}` );
+							execSync( `openssl genrsa -out "${getCertsPath}/${domain}/${domain}.key" 4096`, { stdio: 'ignore' } );
+							execSync( `openssl req -new -key "${getCertsPath}/${domain}/${domain}.key" -out "${getCertsPath}/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test"`, { stdio: 'ignore' } );
+							execSync( `openssl x509 -req -in "${getCertsPath}/${domain}/${domain}.csr" -CA "${getCertsPath}/ca/ca.crt" -CAkey "${getCertsPath}/ca/ca.key" -CAcreateserial -out "${getCertsPath}/${domain}/${domain}.crt" -days 365 -sha256 -extfile "${getCertsPath}/${domain}/${domain}.ext"`, { stdio: 'ignore' } );
+						}
+					} );
+				}
+			} );
 		}
     }
 }
