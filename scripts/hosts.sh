2124b9fdef065ac0e9e182b8820f3bef3775e1d1
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-11 01:18:40 -0700

    updated

---
+++ scripts/hosts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+config="$PWD/.global/custom.yml"
+
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
+
+if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+    if ! grep -q "dashboard.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
+        wp4docker-hosts set 127.0.0.1 dashboard.test
+    fi
+else
+    if ! grep -q "dashboard.test" /etc/hosts; then
+        sudo wp4docker-hosts set 127.0.0.1 dashboard.test
+    fi
+fi
+
+for domain in `get_sites`; do
+  if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+      if ! grep -q "${domain}.test" /mnt/c/Windows/System32/drivers/etc/hosts; then
+          wp4docker-hosts set 127.0.0.1 ${domain}.test
+      fi
+  else
+      if ! grep -q "${domain}.test" /etc/hosts; then
+          sudo wp4docker-hosts set 127.0.0.1 ${domain}.test
+      fi
+  fi
+done
--- src/init.js
+++ src/init.js
@@ -1,7 +1,8 @@
 const path = require( './configure' );
 const getComposeFile = path.setComposeFile();
-
 const { execSync } = require( 'child_process' );
 
 execSync( `docker-compose -f ${getComposeFile} exec nginx sudo mysql -uroot -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';"`, { stdio: 'inherit' } );
 execSync( `docker-compose -f ${getComposeFile} exec nginx sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"`, { stdio: 'inherit' } );
+
+execSync( `bash scripts/hosts.sh` );
