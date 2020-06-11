845972e064ff795e616cc5e2496f118c1772eb06
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-11 10:20:37 -0700

    removed unwanted stuff

--- src/provision.js
+++ src/provision.js
@@ -4,19 +4,7 @@
 // reconfigured, just extended if necessary. Some of these may not needed will
 // be taken out later once this project is complete.
 const path = require( './configure' );
-const getRootPath = path.setRootPath();
-const getCertsPath = path.setCertsPath();
-const getConfigPath = path.setConfigPath();
-const getDatabasesPath = path.setDatabasesPath();
-const getLogsPath = path.setLogsPath();
-const getSitesPath = path.setSitesPath();
-const getSrcPath = path.setSrcPath();
-const isWSL = require( "is-wsl" );
-const { execSync } = require( 'child_process' );
-
-// Here, we will be using the require the js-yaml and fs file and read the .global/docker-custom.yml
-const fs = require( "fs-extra" );
-const yaml = require( "js-yaml" );
+const { execSync, exec } = require( 'child_process' );
 const getComposeFile = path.setComposeFile();
 
 execSync( `docker-compose -f ${getComposeFile} exec nginx make`, { stdio: 'inherit' } );
