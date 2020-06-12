7baee31b03a063044793e17310074d25715acffd
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-12 02:27:57 -0700

    no longer need

--- src/init.js
+++
@@ -1,6 +0,0 @@
-const path = require( './configure' );
-const getComposeFile = path.setComposeFile();
-const { execSync } = require( 'child_process' );
-
-execSync( `docker-compose -f ${getComposeFile} exec nginx sudo mysql -uroot -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';"`, { stdio: 'inherit' } );
-execSync( `docker-compose -f ${getComposeFile} exec nginx sudo mysql -uroot -proot -e "FLUSH PRIVILEGES;"`, { stdio: 'inherit' } );
