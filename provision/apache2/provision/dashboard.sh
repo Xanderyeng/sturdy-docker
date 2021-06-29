7340ecb996e7e60eaf33296565076e76c33fe779
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-06-29 13:06:31 -0700

    updated

--- provision/apache2/provision/dashboard.sh
+++ provision/apache2/provision/dashboard.sh
@@ -29,7 +29,10 @@ if [[ false != "${repo}" ]]; then
         cd /app
     else
         cd ${github}
-        noroot git pull -q
+        noroot git fetch origin main -q
+        noroot rm -rf ${dir}
+        noroot npm run build &> /dev/null
+        noroot mv ${github}/public_html ${dir}
         cd /app
     fi
 fi
