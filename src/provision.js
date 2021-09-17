e7723c5366563c3ff91963b7aeed1c213ebcc6b2
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-09-17 13:31:01 -0700

    updated

--- src/provision.js
+++ src/provision.js
@@ -6,25 +6,29 @@
 const path = require( "../src/configure" );
 const getConfigPath = path.setConfigPath();
 const getGlobalPath = path.setGlobalPath();
+const getComposeFile = path.setComposeFile();
+const getCustomFile = path.setCustomFile();
 
 // Here, we will be using the require the js-yaml and fs file and read the .global/docker-custom.yml
 const fs = require( "fs-extra" );
 const yaml = require( "js-yaml" );
 const { execSync } = require( 'child_process' );
-const getComposeFile = path.setComposeFile();
-const getCustomFile = path.setCustomFile();
 
+// If custom.yml doesn't exist in the .global folder, then copy from default.yml to custom.yml.
 if ( ! fs.existsSync( `${getCustomFile}` ) ) {
     fs.copyFileSync( `${getConfigPath}/default.yml`, `${getGlobalPath}/custom.yml` );
 }
 
+// Here, we are going to load the custom.yml file so that we can begin automation.
 const config = yaml.safeLoad( fs.readFileSync( `${getCustomFile}`, 'utf8' ) );
 
+// Let's create some main variables for each components from the custom.yml.
 const dashboard_defaults = Object.entries( config.default );
 const options_defaults = Object.entries( config.options );
 const sites_defaults = Object.entries( config.sites );
 const resources_defaults = Object.entries( config.resources );
 
+// Here, we are going to setup dashboard.
 for ( const [ name, value ] of dashboard_defaults ) {
     const provision = value.provision;
     const repo = value.repo;
@@ -32,13 +36,14 @@ for ( const [ name, value ] of dashboard_defaults ) {
     execSync( `docker-compose -f ${getComposeFile} exec server bash /app/dashboard.sh ` + name + ' ' + provision + ' ' + repo, { stdio: 'inherit' } );
 }
 
+// Here, we are going to setup options such s db_backups and db_restores.
 for ( const [ name, value ] of options_defaults ) {
-    
     if ( name == 'db_restores' ) {
         execSync( `docker-compose -f ${getComposeFile} exec server bash /app/databases.sh ` + name + ' ' + value, { stdio: 'inherit' } );
     }
 }
 
+// Here, we are going to automate sites.
 for ( const [ name, value ] of sites_defaults ) {
     const provision = value.provision;
     const repo = value.repo;
@@ -46,6 +51,7 @@ for ( const [ name, value ] of sites_defaults ) {
     execSync( `docker-compose -f ${getComposeFile} exec server bash /app/sites.sh ` + name + ' ' + provision + ' ' + repo, { stdio: 'inherit' } );
 }
 
+// Here, we are going to setup resources.
 for ( const [ name, value ] of resources_defaults ) {
     const utilities = Object.entries( value.utilities );
     const repo = value.repo;
@@ -55,8 +61,9 @@ for ( const [ name, value ] of resources_defaults ) {
     }
 }
 
-
+// Here, we are going to restart nginx so that all generated sites are loaded properly.
 execSync( `docker-compose -f ${getComposeFile} exec server sudo service nginx restart > /dev/null 2>&1`, { stdio: 'inherit' } );
 
+// Here, we are going to make sure that the hosts file are setup properly.
 const getWSL = require( '../src/wsl' );
 getWSL.wsl_host();
