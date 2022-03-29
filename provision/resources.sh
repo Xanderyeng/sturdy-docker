6218ae09def0abc7fa7ddacb67f0e48e06abc9ce
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-03-28 20:49:16 -0700

    make sure that we set provision to true so that it generates certificates if not, it shouldn't do shit.

--- provision/resources.sh
+++ provision/resources.sh
@@ -5,14 +5,30 @@ repo=$2
 config="/srv/.global/custom.yml"
 dir="/app/resources"
 
-if [[ ! -z ${name} && ! -z ${repo} ]]; then
-    if [[ ! -d ${dir}/.git ]]; then
-        git clone --branch main ${repo} ${dir} -q
-    else
-        cd ${dir}
-        git pull  -q
-        cd /app
-    fi
-fi
+get_sites() {
+    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
 
-source ${dir}/${name}/setup.sh
+for domain in `get_sites`; do
+    get_site_provision() {
+        local value=`cat ${config} | shyaml get-value sites.${domain}.provision 2> /dev/null`
+        echo ${value:-$@}
+    }
+
+    provision=`get_site_provision`
+
+    if [[ ${provision} == "True" ]]; then 
+        if [[ ! -z ${name} && ! -z ${repo} ]]; then
+            if [[ ! -d ${dir}/.git ]]; then
+                git clone --branch main ${repo} ${dir} -q
+            else
+                cd ${dir}
+                git pull  -q
+                cd /app
+            fi
+        fi
+
+        source ${dir}/${name}/setup.sh
+    fi
+done
