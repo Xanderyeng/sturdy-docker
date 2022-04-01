97323b13d9bc53e09931fad4d7b750403498f768
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-01 16:24:14 -0700

    this should help with adding php and server by default

--- provision/dashboard.sh
+++ provision/dashboard.sh
@@ -4,12 +4,35 @@ server=$1
 domain=$2
 provision=$3
 repo=$4
-dir="/srv/www/${domain}"
+php=$5
+dir=$6
 
 if [[ ${server} == 'nginx' ]]; then
     if [[ ! -d "/etc/nginx/conf.d/${domain}.test.conf" ]]; then
         sudo cp "/srv/config/nginx/nginx.conf" "/etc/nginx/conf.d/${domain}.test.conf"
         sudo sed -i -e "s/{{DOMAIN}}/${domain}/g" "/etc/nginx/conf.d/${domain}.test.conf"
+    fi
+
+    if [[ ! -z "${php}" ]]; then
+        if [[ "${php}" == "7.4" ]]; then
+            if grep -q "8.0" "/etc/nginx/conf.d/${domain}.test.conf"; then
+                sudo sed -i -e "s/8.0/7.4/g" "/etc/nginx/conf.d/${domain}.test.conf"
+            fi
+
+            if grep -q "8.1" "/etc/nginx/conf.d/${domain}.test.conf"; then
+                sudo sed -i -e "s/8.1/7.4/g" "/etc/nginx/conf.d/${domain}.test.conf"
+            fi
+        fi
+
+        if [[ "${php}" == "8" ]]; then
+            if grep -q "7.4" "/etc/nginx/conf.d/${domain}.test.conf"; then
+                sudo sed -i -e "s/7.4/8.0/g" "/etc/nginx/conf.d/${domain}.test.conf"
+            fi
+
+            if grep -q "8.1" "/etc/nginx/conf.d/${domain}.test.conf"; then
+                sudo sed -i -e "s/8.1/8.0/g" "/etc/nginx/conf.d/${domain}.test.conf"
+            fi
+        fi
     fi
 elif [[ ${server} == 'apache2' ]]; then 
     if [[ ! -d "/etc/apache2/sites-available/${domain}.test.conf" ]]; then
