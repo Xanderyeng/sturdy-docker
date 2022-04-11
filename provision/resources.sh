ef74c0ebc8c1a23029c23677de98b6355aadc1e5
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-11 10:02:57 -0700

    updated

--- provision/resources.sh
+++ provision/resources.sh
@@ -2,33 +2,17 @@
 
 name=$1
 repo=$2
-config="/srv/.global/custom.yml"
-dir="/app/resources"
+custom=$3
+dir=$4
 
-get_sites() {
-    local value=`cat ${config} | shyaml keys sites 2> /dev/null`
-    echo ${value:-$@}
-}
-
-for domain in `get_sites`; do
-    get_site_provision() {
-        local value=`cat ${config} | shyaml get-value sites.${domain}.provision 2> /dev/null`
-        echo ${value:-$@}
-    }
-
-    provision=`get_site_provision`
-
-    if [[ ${provision} == "True" ]]; then 
-        if [[ ! -z ${name} && ! -z ${repo} ]]; then
-            if [[ ! -d ${dir}/.git ]]; then
-                git clone --branch main ${repo} ${dir} -q
-            else
-                cd ${dir}
-                git pull  -q
-                cd /app
-            fi
-        fi
-
-        source ${dir}/${name}/setup.sh
+if [[ ! -z ${name} && ! -z ${repo} ]]; then
+    if [[ ! -d ${dir}/.git ]]; then
+        git clone --branch main ${repo} ${dir} -q
+    else
+        cd ${dir}
+        git pull  -q
+        cd /app
     fi
-done
+fi
+
+source ${dir}/${name}/setup.sh
